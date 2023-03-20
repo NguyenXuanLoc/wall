cbe71a2aa232c278ccaffdaaab54586406de5fc5
NguyenXuanLoc <59056264+NguyenXuanLoc@users.noreply.github.com>
2023-03-20 21:49:48 +0700

    Convert json to object

--- Assets/camera.cs
+++ Assets/camera.cs
@@ -1,7 +1,17 @@
+using Defective.JSON;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public class HoldModel
+{
+    public string type;
+    public int rotation;
+   public HoldModel(string type, int rotation)
+    {
+        this.type = type;
+        this.rotation = rotation;
+    }
+}
 public class camera : MonoBehaviour
 {
 
@@ -12,10 +22,27 @@ public class camera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        fakeData();
     }
-
-    // Update is called once per frame
+    void fakeData()
+    {
+        var jsonTextFile = Resources.Load<TextAsset>("JsonData/route");
+        var holdStr = new JSONObject(jsonTextFile.text)["data"]["holds"].ToString();
+        var lHoldJson = new JSONObject(holdStr.Replace("\'", "\"").Substring(1, holdStr.Length - 1));
+        List<List< HoldModel >> lHold = new List<List<HoldModel>>();
+        foreach (JSONObject parent in lHoldJson.list)
+        {
+            List<HoldModel> lSub = new List<HoldModel>();
+            foreach (JSONObject sub in parent.list)
+            {
+                HoldModel holdModel = new HoldModel(sub["type"].ToString(), int.Parse(sub["rotation"].ToString()));
+                lSub.Add(holdModel);
+            }
+            lHold.Add(lSub);
+        }
+        Debug.Log("lParent: " + lHold.Count);
+    }
+    // Update is called once perframe
     void Update()
     {
         x = Input.GetAxis("Mouse X");
@@ -26,3 +53,4 @@ public class camera : MonoBehaviour
         transform.eulerAngles += rotateValue * camSpeed;
     }
 }
+
